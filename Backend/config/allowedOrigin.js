bac20ec02236d2f545053e528e899f15242a5629
Dhruveel3092 <dhruveelgajipara247@gmail.com>
2025-02-17 19:19:43 +0530

    minor change

--- Backend/config/allowedOrigin.js
+++ Backend/config/allowedOrigin.js
@@ -3,10 +3,7 @@ const allowedOrigin=[
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
-    'https://eazy-mess-hi4p-2gqysytw6-dhruveels-projects.vercel.app',
-    'https://eazy-mess-hi4p-iz9zuc11k-dhruveels-projects.vercel.app',
     'https://eazy-mess-hi4p.vercel.app',
-    'https://eazy-mess-hi4p-git-main-dhruveels-projects.vercel.app',
 ]
 
 export default allowedOrigin;
